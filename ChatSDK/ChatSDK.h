805b6da9496786400e0957e8a4ebdab9d9173ab2
Rafael <rafaelopires@icloud.com>
2025-04-09 21:56:48 -0300

    Initial Commit

---
+++ ChatSDK/ChatSDK.h
@@ -0,0 +1,18 @@
+//
+//  ChatSDK.h
+//  ChatSDK
+//
+//  Created by Rafael Pires on 09/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatSDK.
+FOUNDATION_EXPORT double ChatSDKVersionNumber;
+
+//! Project version string for ChatSDK.
+FOUNDATION_EXPORT const unsigned char ChatSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatSDK/PublicHeader.h>
+
+
